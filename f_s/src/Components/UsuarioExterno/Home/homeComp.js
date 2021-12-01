9f66d255dbe8115a37ef2d76c70808804e6ddda5
jeison-AK <50089525+jeison-AK@users.noreply.github.com>
2021-12-01 11:29:04 -0500

    navbar y otras caracteristicas

--- f_s/src/Components/UsuarioExterno/Home/homeComp.js
+++ f_s/src/Components/UsuarioExterno/Home/homeComp.js
@@ -5,6 +5,14 @@ import NewNavBar from "../../Comun/NavBar/NavBar_JM";
 import "../../Comun/NavBar/navBar.css";
 import "../../Comun/footer/footer";
 import Footer from "../../Comun/footer/footer";
+
+//-Esta pagina cubre los siguientes requerimientos para el usuario externo:
+// ❌	Darse de alta en la plataforma (Registrarse).
+// ✅	Generar una orden de despacho indicando el nombre, descripción, y peso del producto. Así mismo, deberá ingresar la cantidad de vehículos necesarios para el transporte, el lugar de recogida y el lugar de entrega.
+// ✅	Ver historial de ordenes de despacho.
+// ✅	Filtrar órdenes de despacho de acuerdo a su estado
+// ✅	Ver detalle de una orden de despacho.
+
 function Home() {
   return (
     <div class="grid">
@@ -12,22 +20,50 @@ function Home() {
         <NewNavBar />
       </nav>
       <main>
-        <section id="mid">
-          <div class="grid">
-            <div class="g-col">1</div>
-            <div class="g-col">2</div>
-            <div class="g-col">2</div>
-            <div class="g-col">2</div>
-            <div class="g-col">2</div>
-            <div class="g-col">2</div>
-            <div class="g-col">2</div>
-            <div class="g-col">2</div>
-            <div class="g-col">2</div>
-            <div class="g-col">2</div>
-            <div class="g-col">2</div>
-            <div class="g-col">2</div>
-            <div class="g-col">2</div>
-            <div class="g-col">2</div>
+        <section id="mid" class="container-fluid">
+          <div class="row">
+            <div class="col-md-8 ">
+              <div class="dropdown">
+                <button
+                  class="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  id="dropdownMenuButton1"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Filtrar estatus
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Pendiente
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Despachada
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Finalizada
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Cancelada
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-4">
+              SideBar
+              <p>
+                para ver la info de la orden que se escoja del filtro y generar
+                la orden
+              </p>
+            </div>
           </div>
         </section>
       </main>
