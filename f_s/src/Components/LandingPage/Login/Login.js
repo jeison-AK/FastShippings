4c786a2e9474a1a7a43141a1da918704e489378f
jeison-AK <50089525+jeison-AK@users.noreply.github.com>
2021-12-10 19:27:00 -0500

    arreglando conflictos

--- f_s/src/Components/LandingPage/Login/Login.js
+++ f_s/src/Components/LandingPage/Login/Login.js
@@ -4,20 +4,28 @@ import Opciones from "../Login/Label/Label";
 import NavBar_CC from "../../Comun/NavBar/NavBar_CC";
 import Footer from "../../Comun/footer/footer";
 
-function Login (){
-    return (
-        <main className="principal">
-            <NavBar_CC />
-            <div>
-                <section className="contenedor-principal">                    
-                    <h1 className="titulo">INICIA SESIÓN</h1>
-                    <Opciones />
-                    <Footer/>
-                </section>           
-            </div>
+function Login() {
+  const [user, setUser] = React.useState(null);
+  //no es un sistema de logeo completo
+  async function login(user = null) {
+    setUser(user);
+  }
+  async function logout() {
+    setUser(null);
+  }
 
-        </main>    
-    )
-};
+  return (
+    <main className="principal">
+      <NavBar_CC />
+      <div>
+        <section className="contenedor-principal">
+          <h1 className="titulo">INICIA SESIÓN</h1>
+          <Opciones />
+          <Footer />
+        </section>
+      </div>
+    </main>
+  );
+}
 
 export default Login;
