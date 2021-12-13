2e758c01ae52fd801c7d17c051973919d13dc4dc
jeison-AK <50089525+jeison-AK@users.noreply.github.com>
2021-12-12 19:09:12 -0500

    Listo para conectar front y back

--- f_s/src/componentsB/CreateNote.jsx
+++ f_s/src/componentsB/CreateNote.jsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useState } from "react"; //para recordar cambios tenemos q tener conciencia de los estados del imput
 
 function CreateNote() {
+  const [input, setInput] = useState({
+    title: "", //field to be tracked
+    content: "", //field to be tracked, para el value de textarea
+  });
+  function handleChange(event) {
+    const { name, value } = event.target; //referencia el name y value q estan en el html
+    setInput((prevInput) => {
+      return {
+        ...prevInput,
+        [name]: value,
+      };
+    });
+  }
+  function handleClick(event) {
+    event.preventDefault(); //prevent refresh after a button click
+    console.log(input);
+  }
   return (
     <div className="container">
       <h1>Create a note 🎵🎶!!</h1>
+      <form action="">
+        <div className="form-group">
+          <input
+            onChange={handleChange}
+            name="title"
+            value={input.title}
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <div>
+          <textarea
+            onChange={handleChange}
+            name="content"
+            value={input.content}
+            id=""
+            cols="30"
+            rows="10"
+            className="form-control"
+          ></textarea>
+        </div>
+        <button onClick={handleClick} className="btn btn-lg btn-info">
+          Add note
+        </button>
+      </form>
     </div>
   );
 }
