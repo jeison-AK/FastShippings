9f58c2786aef834325f69657ac5262fc3bf5c2fc
jeison-AK <50089525+jeison-AK@users.noreply.github.com>
2021-12-14 13:24:00 -0500

    Axios

--- f_s/src/App.js
+++ f_s/src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   BrowserRouter as Router,
   Outlet,
@@ -27,7 +26,6 @@ function App() {
     </Router>
   );
 }
-
 export default App;
 
 function Layout() {
@@ -37,13 +35,3 @@ function Layout() {
     </div>
   );
 }
-//Navigate replace redirecciona
-// no usamos <a> tags porque recarga toda la pagina en su lugar usamos Link
-//outlet indica en qué parte de la pagina cargar algún componente
-//useParams para hacer uso de :id parametros que se pasen en la url
-//element={<>} lo que va aquí son los componentes que injectamos
-//TEST ve al link courses/TuNombre   pon tu nombre allí, observa Courses() y CourseId()
-//NavLink similar a useParams también te redirecciona
-//useNavigate te puedes traer información de alguna pagina pasando el valor en el 2do argumento {state:"value"}, ver CourseId(), Dashboard()
-//no tiene que ser un string {state: value} siempre
-//Podemos usar Link en lugar de useNavigate, aqui el state siempre sera un string
--- f_s/src/Components/testConAxios.js
+++ f_s/src/Components/testConAxios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React from "react";
-import { Link } from "react-router-dom";
 
 const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
 
