a458b61bce905f0ec3070caa2537dbd7d3daa125
jeison-AK <50089525+jeison-AK@users.noreply.github.com>
2021-12-17 07:32:54 -0500

    🟢

--- f_s/src/http-common.js
+++ f_s/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 //! ver services/restaurant.js
 export default axios.create({
-  baseURL: "http://localhost:5000/api/v1/Estados", //url de nuestro servidor backend,la url root
+  baseURL: "http://localhost:5000/api/v1/rutas", //url de nuestro servidor backend,la url root
   headers: {
     "Content-type": "application/json",
   },
